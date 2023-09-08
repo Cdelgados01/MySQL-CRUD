b38d72e97081de988a3ceef01396a0313a872543
cdelg <cdelg@10.203.166.60>
2023-09-08 14:25:54 -0500

    TestMySQL01

---
+++ src/main/java/com/mycompany/mysql01/MySQL01.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.mysql01;
+
+import com.mycompany.mysql01.persistencia.ControladoraPersistencia;
+
+public class MySQL01 {
+
+    public static void main(String[] args) {
+        
+        ControladoraPersistencia controlPersist = new ControladoraPersistencia();
+        
+    }
+}
---
+++ src/main/java/com/mycompany/mysql01/persistencia/ControladoraPersistencia.java
@@ -0,0 +1,8 @@
+
+package com.mycompany.mysql01.persistencia;
+
+public class ControladoraPersistencia {
+    
+    UsuarioJpaController UserJpa = new UsuarioJpaController();
+    
+}
